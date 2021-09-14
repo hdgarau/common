7b087be96bf4d41aa09176adaf28b557701e0fd8
Hernan Daniel Garau <hgarau@provincianet.com.ar>
2021-09-14 12:29:21 -0300

    fix test all to Y-m-d

--- Test/Date/PeriodTest.php
+++ Test/Date/PeriodTest.php
@@ -9,9 +9,9 @@ class PeriodTest extends TestCase
     public function testSimpleMethods()
     {
         $date = \DateTime::createFromFormat('Y-m-d H:i:s','1983-02-15 00:00:00');
-        self::assertEquals($date,Period::days(3,'1983-02-18'));
-        self::assertEquals($date,Period::weeks(1,'1983-02-22'));
-        self::assertEquals($date,Period::months(2,\DateTime::createFromFormat('Y-m-d','1983-04-15')));
+        self::assertEquals($date->format('Y-m-d'),Period::days(3,'1983-02-18')->format('Y-m-d'));
+        self::assertEquals($date->format('Y-m-d'),Period::weeks(1,'1983-02-22')->format('Y-m-d'));
+        self::assertEquals($date->format('Y-m-d'),Period::months(2,\DateTime::createFromFormat('Y-m-d','1983-04-15')));
         Period::setDefault('1985-02-15');
         self::assertEquals($date->format('Y-m-d'),Period::years(2)->format('Y-m-d'));
         self::assertEquals('1985-02-11',Period::weekBegin()->format('Y-m-d'));
